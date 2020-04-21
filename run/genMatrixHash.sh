6950fb1cd21479a5eba4aac831f043d3f6f9fe2b
Venkatesh Sridhar <vsridha@purdue.edu>
2020-04-21 00:44:19 -0400

    Comments added

--- run/genMatrixHash.sh
+++ run/genMatrixHash.sh
@@ -17,7 +17,7 @@
 #    Ex.1: ./genMatrixHash.sh ../data/shepp
 #    Ex.2: ./genMatrixHash.sh ../data/shepp.imgparams ../data/shepp.sinoparams
 
-# parse arguments
+# parse arguments. "$#" represents  no. of arguments beside run-script name
 if [[ "$#" -lt 1 ]]; then
   echo "Not enough arguments"
   exit 1
@@ -39,9 +39,16 @@ fi
 
 # It's forgiving if sino/img arguments are swapped, HOWEVER the views 
 # file path is pulled specifically from the last argument
+
+# The below line is a piped command - executes multiple shell commands seprated by | sequentially
+# 1) cat lists the contents of the file line-by-line ($sinofile should suffice $imgile not needed)
+# 2) grep shows only those lines of the above file that have a particular word or pattern
+# 3) cut splits a given line into 2 parts at each occurence of a delimiter specified by -d option, and, then from all parts selects the one specified by -f
+# 4) tr deletes from a string occurence of a delimiter specified by -d 
 viewsfile="$(cat $imgfile $sinofile |grep ViewAngle |cut -d : -f 2 |tr -d ' ')"
+# File for list of view-angles
 viewsfile="$(dirname $sinofile)/$viewsfile"
-#echo "$viewsfile"
+
 
 # check views list file existance 
 if [[ ! -f "$viewsfile" ]]; then
@@ -51,6 +58,14 @@ fi
 
 # Pull the relevant parameter fields; remove spaces; sort to remove field order 
 # depencence; add views file; generate and truncate sha1 hash
+# In below piped command 
+# 1) grep -e if u have multiple patterns for which relevant lines must be extracted, specify each of them separated by -e option
+# 2) tr -d '[:blank:]' deletes spaces (same as tr -d ' ')
+# 3) sort command sorts above lines in alphabetical order
+# 4) shasum generates a 'checksum code' based on above contents ...
+#    The idea here is each time you make a change to any of the parameters --> you get a unique code
+# 5) cut -c 1-8 selects only first 8 characters from the long checksum code that is generated.
+
 cat "$imgfile" "$sinofile" \
   | grep -e Nx -e Ny -e Deltaxy -e ROIRadius -e NChannels -e NViews -e DeltaChannel -e CenterOffset \
   | tr -d '[:blank:]' \
--- run/runDemo.sh
+++ run/runDemo.sh
@@ -11,29 +11,37 @@
 export OMP_NUM_THREADS=20
 export OMP_DYNAMIC=true
 
+# Changes directory to where run-script (represented here by $0) is
 cd "$(dirname $0)"
 
 ### Set executable and data locations
 execdir="../bin"
 
-dataDir="../../mbir-demos"
+# Default data-directory (for all datasets) and dataset-name. 
+# This can also be altered through additional arguments in command line.
+dataDir="../data"
 dataName="shepp"
-#dataName="xradia"
 
+# Here $# means no. of arguments (excluding run-script). 
+# If no. of arguments > 0, then extra argument is of form "path-for-all-datasets/dataset-name" 
 if [[ "$#" -gt 0 ]]; then
-  dataDir="$(dirname $1)"
+  dataDir="$(dirname $1)" 
   dataName="$(basename $1)"
 fi
 
+# Path for parameter files
 parName="$dataDir/$dataName/par/$dataName"
+# Path for sinogram data
 sinoName="$dataDir/$dataName/sino/$dataName"
 wgtName="$dataDir/$dataName/weight/$dataName"
+# Path for reconstruction
 recName="$dataDir/$dataName/recon/$dataName"
-
-matDir="./sysmatrix"
+# Path for system-matrix
+matDir="$dataDir/sysmatrix"
 matName="$matDir/$dataName"
 
 # create folders that hold pre-computed items and output if they don't exist
+# -d usage: check if directory exists
 if [[ ! -d "$matDir" ]]; then
   echo "Creating directory $matDir"
   mkdir "$matDir"
@@ -63,7 +71,19 @@ fi
 ###   it computes and saves the matrix; If yes, it skips to the reconstruction.
 
 ### PRE-COMPUTE STAGE
+
+# In below script "$?" means exit status. General use is as follows
+#run_some_command
+#EXIT_STATUS=$?
+#if [ "$EXIT_STATUS" -eq "0" ]
+#then
+    ## Do work when command exists on success
+#else
+    ## Do work for when command has a failure exit
+#fi
+
 # generate the hash value and check the exit status
+# The below line invokes a separate run-script on its own
 HASH="$(./genMatrixHash.sh $parName)"
 if [[ $? -eq 0 ]]; then
    matName="$matDir/$HASH"
@@ -72,7 +92,7 @@ else
    [[ -f "$matName.2Dsvmatrix" ]] && /bin/rm "$matName.2Dsvmatrix" 
 fi
 
-# check for matrix file, and compute if not present
+# check for matrix file (-f usage), and compute if not present
 if [[ ! -f "$matName.2Dsvmatrix" ]]; then
    echo "Generating system matrix file: $matName.2Dsvmatrix"
    echo "Generating projection file: $matName.2Dprojection"
